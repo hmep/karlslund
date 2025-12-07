4b21cddb446061f1b27a54f6f3eca33e2db6cf28
Tobias Hagberg <tobias@hagberg.com>
2025-12-07 11:04:22 +0100

    Publicerar v0.10.20 av paint-o-matic till shinyapps.io [deploy]

--- paint-o-matic/app.R
+++ paint-o-matic/app.R
@@ -176,7 +176,7 @@ ui <- dashboardPage(
     # Version number (right side, small text)
     tags$li(
       class = "dropdown",
-      tags$a(href = "https://github.com/hmep/karlslund/blob/main/paint-o-matic/LICENSE", class = "version-text", "v0.10.19-fast, © 2025 Tobias Hagberg, licens GPLv3")
+      tags$a(href = "https://github.com/hmep/karlslund/blob/main/paint-o-matic/LICENSE", class = "version-text", "v0.10.20, © 2025 Tobias Hagberg, licens GPLv3")
     )
   ),
   dashboardSidebar(disable = TRUE),

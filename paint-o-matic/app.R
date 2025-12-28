22ecb507795cfe1abc602118b29ae2ab0c062548
Tobias Hagberg <tobias@hagberg.com>
2025-12-28 19:17:51 +0100

    Publicerar v0.10.39-egg av paint-o-matic till shinyapps.io [deploy]

--- paint-o-matic/app.R
+++ paint-o-matic/app.R
@@ -489,7 +489,7 @@ ui <- dashboardPage(
                                    p("En annan lera som också gör färgen tixotrop och ger en len yta är " ,tags$b("kaolin"), " – som sväller mindre och blir mindre geléartad än bentonit."),
                                    hr(),
                                    sliderInput("egg_extra_binder", "Extra äggolja + vatten + fyllmedel (tunnare färg)", 
-                                               1, 2.5, 1.6, 0.05, post = "× minimum"),
+                                               1, 10, 4.5, 0.05, post = "× minimum"),
                                    p("Reglaget ökar mängden ägg, olja och vatten proportionellt, liksom mängden fyllmedel. Högre värde ger tunnare, mer strykbar färg. Det går bra att utöver detta tillsätta ytterligare vatten till färgen, utifrån hur sugande underlaget är."),
                                    hr(),
                                    p("Måla äggoljetemperan med platt och bred pensel som håller mycket färg, i svepande rörelser i olika riktningar, eller med en fin roller (alltid vått i vått). Vänta till nästa strykning med att rätta till misstag eller luckor i färgen, om du går tillbaka och gör om arbetar du bara fram olja till ytan som blir flammig.")
